f191922c7bce2826cfb28adce522cbfee439fb13
yushaliniepillay <yushalinie97@hotmail.com>
2021-01-25 10:14:27 +0800

    first commit

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '15fac8dd9137a8445cc4e4e02f5f388a'),
