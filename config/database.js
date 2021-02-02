90c4e84d1fe6de493bcfe795c6482d550e9786c3
yushaliniepillay <yushalinie97@hotmail.com>
2021-02-02 14:48:01 +0800

    package.json

--- config/database.js
+++ config/database.js
@@ -1,47 +1,15 @@
-const parse =
-  process.env.NODE_ENV === "development"
-    ? () => ""
-    : require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
-const devConnections = (env) => {
-  return {
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
       },
     },
-  };
-};
-
-const productionConnections = (env) => {
-  return {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-      },
-      options: {
-        ssl: false,
-      },
-    },
-  };
-};
-
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections:
-    env("NODE_ENV") === "development"
-      ? devConnections(env)
-      : productionConnections(env),
+  },
 });
